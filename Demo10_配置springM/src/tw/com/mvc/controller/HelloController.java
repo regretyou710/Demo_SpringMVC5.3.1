882ab100257c6732e832c6f12a364191cea75d15
eason <regretyou710@gmail.com>
2022-05-24 20:29:28 +0800

    202205242029

---
+++ Demo10_配置springM/src/tw/com/mvc/controller/HelloController.java
@@ -0,0 +1,8 @@
+package tw.com.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloController {
+
+}
---
+++ Demo12_訪問指定頁面/src/tw/com/mvc/controller/HelloController.java
@@ -0,0 +1,22 @@
+package tw.com.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	// 訪問路徑由根目錄"/"開始->WEB-INFO/templates/index.html
+	// 前綴:/WEB-INFO/templates/
+	// 後綴:.html
+	@RequestMapping("/") // 將當前的請求與控制器方法來創建映射關係
+	public String index() {
+		// 返回視圖名稱
+		return "index";
+	}
+
+	@RequestMapping("/target") // 與請求路徑保持一致
+	public String goToTarget() {
+		return "target";
+	}
+}
