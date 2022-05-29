8aa0ff93d4e30059b9f554e385ebdaa6e51ae0bc
eason <regretyou710@gmail.com>
2022-05-29 12:06:38 +0800

    202205291206

---
+++ Demo44_SpringMVC視圖ThymeleafView/src/tw/com/mvc/controller/ViewController.java
@@ -0,0 +1,13 @@
+package tw.com.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+
+	@RequestMapping("/testThymeleafView")
+	public String testThymeleafView() {
+		return "success";
+	}
+}
